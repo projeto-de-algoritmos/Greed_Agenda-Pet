6253442d953bd4cf94a8ded469add5235be78d0e
Gabrielly Assuncao <gabriellyassuncaorodrigues@gmail.com>
2023-10-26 13:38:54 -0300

    configurando projeto

---
+++ anatomy/src/dashboard.jsx
@@ -0,0 +1,17 @@
+// import React, { useState, useEffect } from 'react';
+// import './style/dashboard.css';
+
+function Dashboard() {
+  //popup
+
+
+  return (
+    <div className='container'>
+      <div>
+        <h1>hello world</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Dashboard;
