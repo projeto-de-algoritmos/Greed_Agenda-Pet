af0cc8ad4169cb149c3bbbeeac54a28b3555ba35
Madu01 <m4dud01@gmail.com>
2023-10-27 23:33:14 -0300

    adicionando imagem de fundo

--- petshop/src/dashboard.jsx
+++ petshop/src/dashboard.jsx
@@ -1,12 +1,15 @@
 // import React, { useState, useEffect } from 'react';
 // import './style/dashboard.css';
+import './style/dashboard.css';
 
 function Dashboard() {
   //popup
 
 
+
+
   return (
-    <div className='container'>
+    <div className='pagina'>
       <div>
         <h1>hello world</h1>
       </div>
